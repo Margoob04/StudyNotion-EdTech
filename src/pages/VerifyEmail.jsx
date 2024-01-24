352fde055d10b43ca3dad3a44a36b56c0860de5c
Sumon <sumonkhan1081@gmail.com>
2024-01-24 05:47:47 +0530

    testing done

--- src/pages/VerifyEmail.jsx
+++ src/pages/VerifyEmail.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import OTPInput from 'react-otp-input'; 
+import OtpInput from "react-otp-input"; 
 import { BiArrowBack } from 'react-icons/bi';
+import { RxCountdownTimer } from "react-icons/rx";
 import { sendOtp, signUp } from '../services/operations/authAPI';
 import { Link, useNavigate } from 'react-router-dom';
 
@@ -13,9 +14,11 @@ const VerifyEmail = () => {
     const { signupData, loading } = useSelector((state) => state.auth);
  
     useEffect(()=>{
+    // Only allow access of this route when user has filled the signup form
       if(!signupData){
         navigate("/signup")
       }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     },[])
 
     const handleOnSubmit = (e)=>{
@@ -34,23 +37,33 @@ const VerifyEmail = () => {
     }
 
     return (
-        <div>
+        <div className="min-h-[calc(100vh-3.5rem)] grid place-items-center">
             {
                 loading ? <div className='spinner'></div>
                 : (
-                   <div>
-                     <h1>Verify Email</h1>
-                     <p>A verification code has been sent to you. Enter the code below</p>
+                   <div className="max-w-[500px] p-4 lg:p-8">
+                     <h1 className="text-richblack-5 font-semibold text-[1.875rem] leading-[2.375rem]">Verify Email</h1>
+                     <p className="text-[1.125rem] leading-[1.625rem] my-4 text-richblack-100">A verification code has been sent to you. Enter the code below</p>
 
                      {/* form verify email*/}
                      <form onSubmit={handleOnSubmit}>
-                        <OTPInput
+                        <OtpInput
                           value={otp}
                           onChange={setOtp}
                           numInputs={6}
-                          renderInput={(props) => <input {...props} />}
+                          renderInput={(props) => (
+                            <input {...props} placeholder="-" style={{
+                                boxShadow: "inset 0px -1px 0px rgba(255, 255, 255, 0.18)",
+                              }}
+                              className="w-[48px] lg:w-[60px] border-0 bg-richblack-800 rounded-[0.5rem] text-richblack-5 aspect-square text-center focus:border-0 focus:outline-2 focus:outline-yellow-50"
+                            />
+                          )}
+                          containerStyle={{
+                            justifyContent: "space-between",
+                            gap: "0 6px",
+                          }}
                         />
-                        <button type='submit'>
+                        <button type='submit' className="w-full bg-yellow-50 py-[12px] px-[12px] rounded-[8px] mt-6 font-medium text-richblack-900">
                             Verify Email
                         </button>
                      </form>
@@ -62,11 +75,12 @@ const VerifyEmail = () => {
                                 <BiArrowBack /> Back To Login
                             </p>
                         </Link>
+                        <button  className="flex items-center text-blue-100 gap-x-2" onClick={()=>  dispatch(sendOtp(signupData.email,navigate))}>
+                          <RxCountdownTimer /> Resend it
+                       </button>
                    </div>
 
-                   <button onClick={()=> dispatch(sendOtp(signupData.email,navigate))}>
-                       Resend it
-                   </button>
+                   
                 </div>
 
             </div> 
